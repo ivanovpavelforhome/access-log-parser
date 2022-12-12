c36d71714800cb477655c51a569eb90afd41538d
Pavel Ivanov <ivanovpavelforhome@gmail.com>
2022-12-12 11:53:35 +0300

    unit4

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,22 @@
-import java.util.Scanner;
+import java.io.File;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Введите первое число: ");
-        int firstNumber = new Scanner(System.in).nextInt();
-        System.out.println("Введите второе число: ");
-        int secondNumber = new Scanner(System.in).nextInt();
-
-        System.out.println("Сумма: " + (firstNumber+secondNumber) );
-        System.out.println("Разность: " + (firstNumber-secondNumber) );
-        System.out.println("Произведение: " + (firstNumber*secondNumber) );
-        System.out.println("Частное: " + (double)firstNumber/secondNumber );
-
+        int i=1;
+        do {
+            System.out.println("Укажите путь к файлу, который программа затем будет парсить:");
+            String path= new java.util.Scanner(System.in).nextLine();
+            File file = new File(path);
+            boolean fileExists = file.exists();
+            boolean isDirectory = file.isDirectory();
+            if (isDirectory) {
+                System.out.println("Указанный путь является путём к папке, а не к файлу");
+                continue;
+            } else if (fileExists) {
+                System.out.println("Путь указан верно");
+                System.out.println("Это файл номер N: " + i);
+                ++i;
+            } else System.out.println("Путь указан неверно");
+        } while (1==1);
     }
-
 }
